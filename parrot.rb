32a8f52b4eb27167a00ce2d288bf29f661008853
langan.kg Langan <langan.kg@gmail.com>
2020-06-03 11:47:29 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,11 +1,11 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot("Squawk!" phrase = "Pretty Bird!")
+def parrot(noise = "Squawk!")
   puts "#{noise}"
   return "Squawk!"
 end
 
-parrot
+parrot("Pretty Bird!")
 
 #def greeting(name, language="Ruby")
   #puts "Hello, #{name}. We heard you are a great #{language} programmer."
